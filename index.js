1f0d0c76460343a118f739642cf1738fd17f6d95
Josh Bullock <mail@joshb.me>
2020-11-21 11:35:26 -0500

    Simplify handlebars extension

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
+
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.render('main', {layout : 'index'});
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
