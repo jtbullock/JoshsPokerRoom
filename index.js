d40bdbd78ed4b662f348986212a3e3f4efff70d7
Josh Bullock <mail@joshb.me>
2020-11-21 12:51:58 -0500

    Add basic auth

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const {auth, requiresAuth} = require('express-openid-connect');
+const config = require('config');
+
 const app = express();
 const port = 3000;
 
-const handlebars = require('express-handlebars');
+app.use(auth(config.get('auth')));
 
 app.set('view engine', 'hbs');
 
@@ -14,7 +18,21 @@ app.engine('hbs', handlebars({
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('main', {layout : 'index'});
+    res.render('main', {layout: 'index'});
+});
+
+app.get('/secured', (req, res) => {
+
+    if (!req.oidc.isAuthenticated()) {
+        res.send('You are not authorized to view this page.');
+        return;
+    }
+
+    res.render('secured', {layout: 'index'});
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
